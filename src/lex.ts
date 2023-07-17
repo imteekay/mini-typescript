273828e0ecd54639462b5167d9cd0848a6904786
imteekay <leandrotk100@gmail.com>
2023-07-17 19:17:10 -0300

    Add open and close brace tokens for the lexer

--- src/lex.ts
+++ src/lex.ts
@@ -59,6 +59,12 @@ export function lex(s: string): Lexer {
         case ',':
           token = Token.Comma;
           break;
+        case '{':
+          token = Token.OpenBrace;
+          break;
+        case '}':
+          token = Token.CloseBrace;
+          break;
         default:
           token = Token.Unknown;
           break;
