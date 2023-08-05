121f2b5c2808d8e5adde232d65c145cec0b7df54
imteekay <leandrotk100@gmail.com>
2023-08-05 15:19:31 -0300

    Test binder: help symbol table debugging

---
+++ src/testBinder.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import { Module, Node, Table } from './types';
+import { parse } from './parse';
+import { lex } from './lex';
+import { bind } from './bind';
+
+const args = process.argv.slice(2);
+const file = args[0];
+
+if (!file) {
+  console.log('Missing test file');
+  process.exit();
+}
+
+function display(o: any) {
+  const o2 = {} as any;
+  for (const k in o) {
+    if (k === 'pos') continue;
+    else if (k === 'kind') o2[k] = Node[o.kind];
+    else if (typeof o[k] === 'object') o2[k] = display(o[k]);
+    else o2[k] = o[k];
+  }
+  return o2;
+}
+
+function displayTable(table: Table) {
+  const o = {} as any;
+  for (const [k, v] of table) {
+    o[k] = v.declarations.map(({ kind, pos }) => ({ kind: Node[kind], pos }));
+  }
+  return o;
+}
+
+function displayModule(m: Module) {
+  return {
+    locals: displayTable(m.locals),
+    statements: m.statements.map(display),
+  };
+}
+
+const tree = parse(lex(fs.readFileSync('tests/' + file, 'utf8')));
+bind(tree);
+
+console.log(JSON.stringify(displayModule(tree), null, 2));
